9a7b5d830d5066146337c41df84de75541883979
Alec Thomas <alec@swapoff.org>
2016-03-09 12:00:39 +1100

    Use different strategies for background and save linting.

--- linter.py
+++ linter.py
@@ -11,8 +11,11 @@
 """This module exports the Gometalinter plugin class."""
 
 import os
+import shlex
+import subprocess
 
 from SublimeLinter.lint import Linter, highlight, util
+from SublimeLinter.lint.persist import settings
 
 
 class Gometalinter(Linter):
@@ -39,5 +42,23 @@ class Gometalinter(Linter):
             print('sublimelinter: using system GOPATH={}'.format(os.environ.get('GOPATH', '')))
 
     def run(self, cmd, code):
+        if settings.get('lint_mode') == 'background':
+            return self._live_lint(cmd, code)
+        else:
+            return self._in_place_lint(cmd)
+
+    def _live_lint(self, cmd, code):
         files = [f for f in os.listdir(os.path.dirname(self.filename)) if f.endswith('.go')]
         return self.tmpdir(cmd, files, code)
+
+    def _in_place_lint(self, cmd):
+        try:
+            filename = os.path.basename(self.filename)
+            cmd = cmd + ['-I', filename]
+            print('gometalinter: linting {}: {}'.format(filename, ' '.join(map(shlex.quote, cmd))))
+            output = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            if e.returncode > 1:
+                print('sublimelinter: some gometalinter sub-linters failed')
+            output = e.output
+        return output.decode('utf-8', errors='replace')
